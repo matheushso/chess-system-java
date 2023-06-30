ee967b29417e19b4f6b01da2092fcbe6a80cb340
Matheus Oliveira <matheushso99@hotmail.com>
2023-06-29 21:19:50 -0300

    Configuring unit tests in the project

---
+++ src/test/java/unit/boardgame/PositionTest.java
@@ -0,0 +1,15 @@
+package unit.boardgame;
+
+import br.com.chess.system.boardgame.Position;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PositionTest {
+
+    @Test
+    public void toString_shouldReturnRowAndColumn() {
+        Position pos = new Position(3, 5);
+        assertEquals("3, 5", pos.toString());
+    }
+}
