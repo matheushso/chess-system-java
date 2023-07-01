d5a4ce03340ea2b4a0368b6f7e9adf36f770b805
Matheus Oliveira <matheushso99@hotmail.com>
2023-07-01 12:42:18 -0300

    Creating position tests

--- src/test/java/br/com/chess/system/unit/boardgame/PositionTest.java
+++ src/test/java/br/com/chess/system/unit/boardgame/PositionTest.java
@@ -7,9 +7,33 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PositionTest {
 
+    private Position pos = new Position(3, 5);
+
+
+    @Test
+    public void getRow_shouldReturnRow() {
+        assertEquals(3, pos.getRow());
+    }
+
+    @Test
+    public void setRow_shouldModifyRow() {
+        pos.setRow(5);
+        assertEquals(5, pos.getRow());
+    }
+
+    @Test
+    public void getColumn_shouldReturnColumn() {
+        assertEquals(5, pos.getColumn());
+    }
+
+    @Test
+    public void setColumn_shouldModifyColumn() {
+        pos.setColumn(7);
+        assertEquals(7, pos.getColumn());
+    }
+
     @Test
     public void toString_shouldReturnRowAndColumn() {
-        Position pos = new Position(3, 5);
         assertEquals("3, 5", pos.toString());
     }
 }
