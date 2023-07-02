afe8dd18998703e6a90c18af717c722f4bd25df8
Matheus Oliveira <matheushso99@hotmail.com>
2023-07-02 11:20:51 -0300

    Chess layer and print the board

--- src/main/java/br/com/chess/system/application/Program.java
+++ src/main/java/br/com/chess/system/application/Program.java
@@ -6,6 +6,6 @@ public class Program {
 
     public static void main(String[] args) {
         ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
+        System.out.println(UI.printBoard(chessMatch.getPieces()));
     }
 }
