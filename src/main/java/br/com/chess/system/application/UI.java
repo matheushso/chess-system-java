af4b1aec29669aba859ed52f556ea181d16e6a75
Matheus Oliveira <matheushso99@hotmail.com>
2023-07-05 23:12:07 -0300

    Improvement in board printing

--- src/main/java/br/com/chess/system/application/UI.java
+++ src/main/java/br/com/chess/system/application/UI.java
@@ -1,16 +1,36 @@
 package br.com.chess.system.application;
 
 import br.com.chess.system.chess.ChessPiece;
+import br.com.chess.system.chess.Color;
 
 public class UI {
+
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
+    public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+    public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+    public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+    public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+    public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+    public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+    public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
     public static StringBuilder printBoard(ChessPiece[][] pieces) {
         StringBuilder board = new StringBuilder();
 
-        for (int i=0; i < pieces.length; i++) {
+        for (int i = 0; i < pieces.length; i++) {
             board.append((8 - i) + " ");
 
-            for (int j=0; j < pieces.length; j++) {
+            for (int j = 0; j < pieces.length; j++) {
                 board.append(printPiece(pieces[i][j]));
             }
 
@@ -28,9 +48,12 @@ public class UI {
         if (piece == null) {
             boardPiece.append("-");
         } else {
-            boardPiece.append(piece);
+            if (piece.getColor() == Color.WHITE) {
+                boardPiece.append(piece);
+            } else {
+                boardPiece.append(ANSI_YELLOW + piece + ANSI_RESET);
+            }
         }
-
         boardPiece.append(" ");
 
         return boardPiece;
