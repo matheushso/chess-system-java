fa340cd690edc615ca2a587cb60ce865d6994e65
Matheus Oliveira <matheushso99@hotmail.com>
2023-08-08 14:35:47 -0300

    Fix move of black pawn

--- src/main/java/br/com/chess/system/chess/pieces/Pawn.java
+++ src/main/java/br/com/chess/system/chess/pieces/Pawn.java
@@ -45,7 +45,7 @@ public class Pawn extends ChessPiece {
             }
 
             position.setValues(this.position.getRow() + 2, this.position.getColumn());
-            Position position2 = new Position(this.position.getRow() - 1, this.position.getColumn());
+            Position position2 = new Position(this.position.getRow() + 1, this.position.getColumn());
             if (getBoard().positionExists(position) && !getBoard().thereIsAPiece(position) && getBoard().positionExists(position2) && !getBoard().thereIsAPiece(position2) && getMoveCount() == 0) {
                 mat[position.getRow()][position.getColumn()] = true;
             }
